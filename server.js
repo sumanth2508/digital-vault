0deda5ba70e8cfb6bc4d693167d30b9b3f3b3655
Sumanth Reddy <mereddysumanth88@gmail.com>
2026-05-28 07:04:02 -0400

    installed mongoose and established a secure connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
 const PORT = 3000;
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+    .then((success) => console.log("Connected to MongoDB"))
+    .catch((err) => console.timeLog("Error connecting to MongoDB"));
+
+
 
 
 app.get("/", (req, res) => {
